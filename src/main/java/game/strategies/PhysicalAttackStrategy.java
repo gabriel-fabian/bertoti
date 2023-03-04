f266064e744e093c61de33df448680711664fdd0
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-03-04 18:00:54 -0300

    refactor: update PhysicalAttackStrategy to deconsider target resistance

--- src/main/java/game/strategies/PhysicalAttackStrategy.java
+++ src/main/java/game/strategies/PhysicalAttackStrategy.java
@@ -6,9 +6,7 @@ public class PhysicalAttackStrategy implements AttackStrategy {
   @Override
   public void attack(Creature attacker, Creature target) {
     int damageAmount = attacker.getDamage("PHYSICAL");
-    int resistanceAmount = target.getResistance("PHYSICAL");
-    int actualDamage = Math.max(damageAmount - resistanceAmount, 0);
 
-    target.takeDamage(actualDamage, "PHYSICAL");
+    target.takeDamage(damageAmount, "PHYSICAL");
   }
 }
