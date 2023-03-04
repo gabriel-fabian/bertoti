905bbe78663553d841a242128c6754085497894b
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-03-04 16:12:17 -0300

    feat: add Creature abstract class

---
+++ src/main/java/game/characters/Creature.java
@@ -0,0 +1,59 @@
+package src.main.java.game.characters;
+
+import java.util.HashMap;
+
+import src.main.java.game.strategies.AttackStrategy;
+
+public abstract class Creature {
+  private String name;
+  private int health;
+  private HashMap<String, Integer> damages;
+  private HashMap<String, Integer> resistances;
+  private AttackStrategy attackStrategy;
+
+  public Creature(String name, int health, HashMap<String,Integer> damages, HashMap<String,Integer> resistances, AttackStrategy attackStrategy) {
+    this.name = name;
+    this.health = health;
+    this.damages = damages;
+    this.resistances = resistances;
+    this.attackStrategy = attackStrategy;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getHealth() {
+    return health;
+  }
+
+  public void setHealth(int health) {
+    this.health = health;
+  }
+
+  public int getDamage(String damageType) {
+    return damages.getOrDefault(damageType, 0);
+  }
+
+  public int getResistance(String damageType) {
+    return resistances.getOrDefault(damageType, 0);
+  }
+
+  public boolean isAlive() {
+    return health > 0;
+  }
+
+  public void takeDamage(int damageAmount, String damageType) {
+    int resistanceAmount = resistances.getOrDefault(damageType, 0);
+    int actualDamage = Math.max(damageAmount - resistanceAmount, 0);
+    health -= actualDamage;
+  }
+
+  public void setAttackStrategy(AttackStrategy attackStrategy) {
+    this.attackStrategy = attackStrategy;
+  }
+
+  public void attack(Creature target) {
+    attackStrategy.attack(this, target);
+  }
+}
